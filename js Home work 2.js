63253d49d8cd206e35fa05a51620111ec77329b7
diiniik5 <nikishina.dasha98@gmail.com>
2022-10-27 23:56:16 +0300

    hw2

---
+++ js Home work 2.js
@@ -0,0 +1,51 @@
+//Возведение 10 в 2 степень, варианты:
+
+//1)
+
+// let i = 1;
+// let result = 1
+// while(i < 11) {
+//     result = result *2
+// i++
+// }
+// console.log(result)
+
+// //2)
+
+// const power = function(num) {
+//     let result = 1;
+//     for (let i =1;i <= num ; i++){
+//         result = result*2
+//     }
+//     return result
+// }
+// console.log(power(10))
+
+// //3)
+
+// const power2 = function(num) {
+//     return 2**num
+// }
+// console.log(power2(10))
+
+// //4)
+
+// const power3 = function(num) {
+//     return Math.pow(2,num)
+// }
+// console.log(power3(10))
+
+
+// работа split
+
+console.log('Maks and Vova, Lera, Katya and Masha' .split('and'))
+
+// работа join
+
+console.log([1,2,3,4,5] .join(' :) '))
+
+
+
+
+
+
